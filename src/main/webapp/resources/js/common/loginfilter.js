018f95ce6b3161019db2b281d7f4cd61f5d6c691
invalided <482149400@qq.com>
2022-08-14 15:56:05 +0800

    remove static resources

--- src/main/webapp/resources/js/common/loginfilter.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-	var userinfo = JSON.parse(localStorage.getItem("userinfo"));
-	if(userinfo == null || userinfo == undefined){
-		$.toast('未登录');
-		window.location.href = '/o2o/local/login';
-	}
-});
--- src/main/webapp/resources/js/frontend/package.js
+++
@@ -1,8 +0,0 @@
-$(".col-33").click(
-	function(){
-		$(".col-33").css({"background":"white","color":"black"});
-		//设置多个css属性
-		$(this).css({"background":"skyblue","color":"white"});
-		
-	}
-);
